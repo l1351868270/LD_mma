50271f1284e1dc66ae189d9fb1e5234c318eebbc
lishuliang <lishuliang@kernelsoft.com>
2024-01-29 18:36:35 +0800

    compelete warp mutmul

--- csrc/mma/src/warp_matmul.h
+++ csrc/mma/src/warp_matmul.h
@@ -40,17 +40,11 @@ __global__ void WarpMatmulForwardV8(void *Cptr, void *Aptr, void *Bptr,
 
   using elem_type = typename Warp_traits::elem_type;
 
-  elem_type* A = (elem_type*)Aptr;
-  elem_type* B = (elem_type*)Bptr;
-  elem_type* C = (elem_type*)Cptr;
+  char* A = (char*)Aptr;
+  char* B = (char*)Bptr;
+  char* C = (char*)Cptr;
 
   int BYTES_PER_ELT = sizeof(elem_type);
-
-  #ifdef DEBUG_WARP_MATMUL_DEVICE
-      if (tidx == 0) {
-          printf("WarpMatmulForwardV8: BYTES_PER_ELT size is: %d\n", BYTES_PER_ELT);
-      }
-  #endif
 
   int ELTS_PER_THREADS = 8;
 
@@ -62,45 +56,42 @@ __global__ void WarpMatmulForwardV8(void *Cptr, void *Aptr, void *Bptr,
   int goffset_b = 0;
   int goffset_c = 0;
 
-    int threads_layout_a[2] = {kTile_M, kTile_K / ELTS_PER_THREADS};
-    int threads_layout_b[2] = {kTile_K, kTile_N / ELTS_PER_THREADS};
-    int threads_layout_c[2] = {kTile_M, kTile_N / ELTS_PER_THREADS};
+  int threads_layout_a[2] = {kTile_M, kTile_K / ELTS_PER_THREADS};
+  int threads_layout_b[2] = {kTile_K, kTile_N / ELTS_PER_THREADS};
+  int threads_layout_c[2] = {kTile_M, kTile_N / ELTS_PER_THREADS};
 
-    int thread_coordinate_a[2] = {tidx / threads_layout_a[1], tidx % threads_layout_a[1]};
-    int thread_coordinate_b[2] = {tidx / threads_layout_b[1], tidx % threads_layout_b[1]};
-    int thread_coordinate_c[2] = {tidx / threads_layout_c[1], tidx % threads_layout_c[1]};
+  int thread_coordinate_a[2] = {tidx / threads_layout_a[1], tidx % threads_layout_a[1]};
+  int thread_coordinate_b[2] = {tidx / threads_layout_b[1], tidx % threads_layout_b[1]};
+  int thread_coordinate_c[2] = {tidx / threads_layout_c[1], tidx % threads_layout_c[1]};
 
-    int sstride_a[2] = {kTile_K, 1};
-    int sstride_b[2] = {kTile_N, 1};
-    int sstride_c[2] = {kTile_N, 1};
+  int sstride_a[2] = {kTile_K, 1};
+  int sstride_b[2] = {kTile_N, 1};
+  int sstride_c[2] = {kTile_N, 1};
 
-    int soffset_a = 0;
-    int soffset_b = 0;
-    int soffset_c = 0;
-    soffset_a += thread_coordinate_a[0] * sstride_a[0];
-    soffset_a += thread_coordinate_a[1] * sstride_a[1] * ELTS_PER_THREADS;
-    soffset_b += thread_coordinate_b[0] * sstride_b[0];
-    soffset_b += thread_coordinate_b[1] * sstride_b[1] * ELTS_PER_THREADS;
-    soffset_c += thread_coordinate_c[0] * sstride_c[0];
-    soffset_c += thread_coordinate_c[1] * sstride_c[1] * ELTS_PER_THREADS;
+  int soffset_a = 0;
+  int soffset_b = 0;
+  int soffset_c = 0;
+  soffset_a += thread_coordinate_a[0] * sstride_a[0] * BYTES_PER_ELT;
+  soffset_a += thread_coordinate_a[1] * sstride_a[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
+  soffset_b += thread_coordinate_b[0] * sstride_b[0] * BYTES_PER_ELT;
+  soffset_b += thread_coordinate_b[1] * sstride_b[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
+  soffset_c += thread_coordinate_c[0] * sstride_c[0] * BYTES_PER_ELT;
+  soffset_c += thread_coordinate_c[1] * sstride_c[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
 
-    int ld_offset_a = 0;
-    int ld_offset_b = 0;
-    int ld_offset_c = 0;
+  int ld_offset_a = 0;
+  int ld_offset_b = 0;
+  int ld_offset_c = 0;
 
-    int ld_thread_coordinate_a[2] = {tidx % 16, tidx / 16};
-    int ld_thread_coordinate_b[2] = {tidx % 16, tidx / 16};
-    int ld_thread_coordinate_c[2] = {tidx % 16, tidx / 16};
+  int ld_thread_coordinate_a[2] = {tidx % 16, tidx / 16};
+  int ld_thread_coordinate_b[2] = {tidx % 16, tidx / 16};
+  int ld_thread_coordinate_c[2] = {tidx % 16, tidx / 16};
 
-    ld_offset_a += ld_thread_coordinate_a[0] * sstride_a[0];
-    ld_offset_a += ld_thread_coordinate_a[1] * sstride_a[1] * ELTS_PER_THREADS;
-    ld_offset_b += ld_thread_coordinate_b[0] * sstride_b[0];
-    ld_offset_b += ld_thread_coordinate_b[1] * sstride_b[1] * ELTS_PER_THREADS;
-    ld_offset_c += ld_thread_coordinate_c[0] * sstride_c[0];
-    ld_offset_c += ld_thread_coordinate_c[1] * sstride_c[1] * ELTS_PER_THREADS;
-
-
-  // // int BYTES_PER_THREADS = BYTES_PER_ELT * ELTS_PER_THREADS;
+  ld_offset_a += ld_thread_coordinate_a[0] * sstride_a[0] * BYTES_PER_ELT;
+  ld_offset_a += ld_thread_coordinate_a[1] * sstride_a[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
+  ld_offset_b += ld_thread_coordinate_b[0] * sstride_b[0] * BYTES_PER_ELT;
+  ld_offset_b += ld_thread_coordinate_b[1] * sstride_b[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
+  ld_offset_c += ld_thread_coordinate_c[0] * sstride_c[0] * BYTES_PER_ELT;
+  ld_offset_c += ld_thread_coordinate_c[1] * sstride_c[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
 
   extern __shared__ char smem_[];
   char* smem_a = smem_;
@@ -109,135 +100,130 @@ __global__ void WarpMatmulForwardV8(void *Cptr, void *Aptr, void *Bptr,
   // char* smem_tmp = smem_ + BYTES_PER_ELT * MMA_M * MMA_K + BYTES_PER_ELT * MMA_K * MMA_N + BYTES_PER_ELT * MMA_M * MMA_N; 
 
   int K_tiles = K / kTile_K;
-  // // int ELTS_PER_ROW_a = K;
-  // // int BYTES_PER_ROW_a = BYTES_PER_ELT * ELTS_PER_ROW_a;
-  // // int TITLE_THREADS_PER_ROW_a = (BYTES_PER_ELT * MMA_K + BYTES_PER_THREADS - 1) / BYTES_PER_THREADS;
-
-  // // int ELTS_PER_ROW_b = N;
-  // // int BYTES_PER_ROW_b = BYTES_PER_ELT * ELTS_PER_ROW_b;
-  // // int TITLE_THREADS_PER_ROW_b = (BYTES_PER_ELT * MMA_N + BYTES_PER_THREADS - 1) / BYTES_PER_THREADS;
-
-  
 
   // // int laneid = lane_id();
 
   int groupID = tidx / 4;
   int threadID_in_group = tidx % 4;
 
-  uint2 s_c = make_uint2(0, 0);
+  #ifdef DEBUG_WARP_MATMUL_DEVICE
+      if (tidx == 0) {
+          printf("WarpMatmulForwardV8: BYTES_PER_ELT K_tiles is: %d\n", K_tiles);
+      }
+  #endif
 
+  uint2 s_c = make_uint2(0, 0);
   for (int Tile_k = 0; Tile_k < K_tiles; Tile_k++) {
     // Load data from global memory to shared memory
 
     // 1. offset - global memory pointer to the Tile Block
-    goffset_a += Tile_m * kTile_M * gstride_a[0];
-    goffset_a += Tile_k * kTile_K * gstride_a[1];
+    goffset_a = 0;
+    goffset_b = 0;
 
-    goffset_b += Tile_k * kTile_K * gstride_b[0];
-    goffset_b += Tile_n * kTile_N * gstride_b[1];
+    goffset_a += Tile_m * kTile_M * gstride_a[0] * BYTES_PER_ELT;
+    goffset_a += Tile_k * kTile_K * gstride_a[1] * BYTES_PER_ELT;
+
+    goffset_b += Tile_k * kTile_K * gstride_b[0] * BYTES_PER_ELT;
+    goffset_b += Tile_n * kTile_N * gstride_b[1] * BYTES_PER_ELT;
 
     // 2. offset - global memory pointer to Thread Block
     // per thread load 8 elements of a row
     // thread
 
-    goffset_a += thread_coordinate_a[0] * gstride_a[0];
-    goffset_a += thread_coordinate_a[1] * gstride_a[1] * ELTS_PER_THREADS;
+    goffset_a += thread_coordinate_a[0] * gstride_a[0] * BYTES_PER_ELT;
+    goffset_a += thread_coordinate_a[1] * gstride_a[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
 
-    goffset_b += thread_coordinate_b[0] * gstride_b[0];
-    goffset_b += thread_coordinate_b[1] * gstride_b[1] * ELTS_PER_THREADS;
+    goffset_b += thread_coordinate_b[0] * gstride_b[0] * BYTES_PER_ELT;
+    goffset_b += thread_coordinate_b[1] * gstride_b[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
 
     uint4 dst_a = make_uint4(0, 0, 0, 0);
     
     if (thread_coordinate_a[0] < threads_layout_a[0]) {
-      printf("goffset_a is: %d, soffset_a is: %d\n", goffset_a * BYTES_PER_ELT, soffset_a * BYTES_PER_ELT);
-      dst_a = *reinterpret_cast<const uint4*>(A + goffset_a * BYTES_PER_ELT);
-      *reinterpret_cast<uint4*>(smem_a + soffset_a * BYTES_PER_ELT) = dst_a;
+      dst_a = *reinterpret_cast<const uint4*>(A + goffset_a);
+      *reinterpret_cast<uint4*>(smem_a + soffset_a) = dst_a;
     }
 
-    // uint4 dst_b = make_uint4(0, 0, 0, 0); 
+    uint4 dst_b = make_uint4(0, 0, 0, 0); 
     
-    // if (thread_coordinate_b[0] < threads_layout_b[0]) {
-    //   dst_b = *reinterpret_cast<const uint4*>(A + goffset_b * BYTES_PER_ELT);
-    //   *reinterpret_cast<uint4*>(smem_b + soffset_b * BYTES_PER_ELT) = dst_b;
-    // }
+    if (thread_coordinate_b[0] < threads_layout_b[0]) {
+      dst_b = *reinterpret_cast<const uint4*>(B + goffset_b);
+      *reinterpret_cast<uint4*>(smem_b + soffset_b) = dst_b;
+    }
 
-    // __syncthreads();
+    __syncthreads();
 
-    // uint4 s_a = make_uint4(0, 0, 0, 0);
+    uint4 s_a = make_uint4(0, 0, 0, 0);
 
-    // uint32_t tt_a = __nvvm_get_smem_pointer(smem_a + ld_offset_a * BYTES_PER_ELT);
-    // asm volatile("ldmatrix.sync.aligned.m8n8.x4.shared.b16 {%0, %1, %2, %3}, [%4];\n"
-    //     : "=r"(s_a.x), "=r"(s_a.y), "=r"(s_a.z), "=r"(s_a.w) 
-    //     : "r"(tt_a));
+    uint32_t tt_a = __nvvm_get_smem_pointer(smem_a + ld_offset_a);
+    asm volatile("ldmatrix.sync.aligned.m8n8.x4.shared.b16 {%0, %1, %2, %3}, [%4];\n"
+        : "=r"(s_a.x), "=r"(s_a.y), "=r"(s_a.z), "=r"(s_a.w) 
+        : "r"(tt_a));
 
-    // uint2 s_b = make_uint2(0, 0);
+    uint2 s_b = make_uint2(0, 0);
 
     
-    // uint32_t tt_b = __nvvm_get_smem_pointer(smem_b + ld_offset_b * BYTES_PER_ELT);
-    // asm volatile("ldmatrix.sync.aligned.m8n8.x2.trans.shared.b16 {%0, %1}, [%2];\n"
-    //     : "=r"(s_b.x), "=r"(s_b.y) 
-    //     : "r"(tt_b));
+    uint32_t tt_b = __nvvm_get_smem_pointer(smem_b + ld_offset_b);
+    asm volatile("ldmatrix.sync.aligned.m8n8.x2.trans.shared.b16 {%0, %1}, [%2];\n"
+        : "=r"(s_b.x), "=r"(s_b.y) 
+        : "r"(tt_b));
 
-    // asm volatile( \
-    //         "mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16 \n" \
-    //         "    {%0, %1}, \n" \
-    //         "    {%2, %3, %4, %5}, \n" \
-    //         "    {%6, %7}, \n" \
-    //         "    {%8, %9}; \n" \
-    //                 : "=r"(  s_c.x), "=r"(  s_c.y)
-    //                 :  "r"(s_a.x),  "r"(s_a.y),  "r"(s_a.z),  "r"(s_a.w)
-    //                 ,  "r"(s_b.x),  "r"(s_b.y)
-    //                 ,  "r"(s_c.x),  "r"(s_c.y));
+    __syncthreads();
 
+  //   asm volatile( \
+  //           "mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16 \n" \
+  //           "    {%0, %1}, \n" \
+  //           "    {%2, %3, %4, %5}, \n" \
+  //           "    {%6, %7}, \n" \
+  //           "    {%8, %9}; \n" \
+  //                   : "=r"(  s_c.x), "=r"(  s_c.y)
+  //                   :  "r"(s_a.x),  "r"(s_a.y),  "r"(s_a.z),  "r"(s_a.w)
+  //                   ,  "r"(s_b.x),  "r"(s_b.y)
+  //                   ,  "r"(s_c.x),  "r"(s_c.y));
+      asm volatile(
+      "mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16 "
+      "{%0,  %1},"
+      "{%2,  %3,  %4,  %5},"
+      "{%6,  %7},"
+      "{%8,  %9};\n"
+      : "=r"(s_c.x), "=r"(s_c.y)
+      :  "r"(s_a.x),  "r"(s_a.y),  "r"(s_a.z),  "r"(s_a.w),
+         "r"(s_b.x),  "r"(s_b.y),
+         "r"(s_c.x),  "r"(s_c.y));
   }
 
-  // // char* 
 
-  // // asm volatile( \
-  // //       "stmatrix.sync.aligned.m8n8.x2.shared.b16 [%0], {%1, %2};\n"
-  // //       : "r"(smem_int_ptr),
-  // //       "r"(s_c.x),  "r"(s_c.y)
-  // // );
 
-  // *(reinterpret_cast<uint32_t*>(smem_acc) + groupID * 4 + threadID_in_group) = s_c.x;
-  // *(reinterpret_cast<uint32_t*>(smem_acc) + 32 + groupID * 4 + threadID_in_group) = s_c.y;
+  __syncthreads();
+  // asm volatile ("stmatrix.sync.aligned.x2.m8n8.shared.b16 [%0], {%1, %2};\n"
+  //       : "=r"(smem_acc + ld_offset_c),
+  //          "r"(s_c.x), "r"(s_c.y));
 
-  // __syncthreads();
+  *(reinterpret_cast<uint32_t*>(smem_acc) + groupID * 4 + threadID_in_group) = s_c.x;
+  *(reinterpret_cast<uint32_t*>(smem_acc) + 32 + groupID * 4 + threadID_in_group) = s_c.y;
 
-  // goffset_c += Tile_m * kTile_M * gstride_c[0];
-  // goffset_c += Tile_n * kTile_N * gstride_c[1];
+  __syncthreads();
 
-  // // __half sc_0 = *reinterpret_cast<__half*>(smem_acc + 4 * groupID *4 + 4* threadID_in_group);
-  // // __half sc_1 = *reinterpret_cast<__half*>(smem_acc + 4 * groupID *4 + 4* threadID_in_group + 2);
-  // // __half sc_2 = *reinterpret_cast<__half*>(smem_acc + 32 * 4 + 4 * groupID *4 + 4* threadID_in_group);
-  // // __half sc_3 = *reinterpret_cast<__half*>(smem_acc + 32 * 4 + 4 * groupID *4 + 4* threadID_in_group + 2);
-  // // printf("ROW: %d, COL %d, ELTS_PER_ROW_c %d THREADS_PER_ROW_c %d, row_acc %d col_acc %d offset %d sc_0 %f sc_1 %f sc_2 %f sc_3 %f\n",
-  // //       ROW, COL, ELTS_PER_ROW_c, TITLE_THREADS_PER_ROW_c, row_acc, col_acc, offset, __half2float(sc_0),  __half2float(sc_1),
-  // //       __half2float(sc_2), __half2float(sc_3));
+  goffset_c += Tile_m * kTile_M * gstride_c[0] * BYTES_PER_ELT;
+  goffset_c += Tile_n * kTile_N * gstride_c[1] * BYTES_PER_ELT;
+
+  goffset_c += thread_coordinate_c[0] * gstride_c[0] * BYTES_PER_ELT;
+  goffset_c += thread_coordinate_c[1] * gstride_c[1] * ELTS_PER_THREADS * BYTES_PER_ELT;
   
-  // // // if (row_acc < MMA_M && col_acc < MMA_N) {
-  //   uint4 t_a = *(reinterpret_cast<uint4*>(C + goffset_c * BYTES_PER_ELT));
-  //   if (thread_coordinate_c[0] < threads_layout_c[0]) {
-  //       // uint4 t_b = *(reinterpret_cast<uint4*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT));
-  //       // __half sc_0 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 0);
-  //       // __half sc_1 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 2);
-  //       // __half sc_2 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 4);
-  //       // __half sc_3 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 6);
-  //       // __half sc_4 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 8);
-  //       // __half sc_5 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 10);
-  //       // __half sc_6 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 12);
-  //       // __half sc_7 = *reinterpret_cast<__half*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT + 14);
-  //       // printf("ROW: %d, COL %d, ELTS_PER_ROW_c %d THREADS_PER_ROW_c %d, row_acc %d col_acc %d offset %d sc_0 %f sc_1 %f sc_2 %f sc_3 %f sc_4 %f sc_5 %f sc_6 %f sc_7 %f\n",
-  //       //   ROW, COL, ELTS_PER_ROW_c, TITLE_THREADS_PER_ROW_c, row_acc, col_acc, offset, __half2float(sc_0),  __half2float(sc_1),
-  //       //    __half2float(sc_2), __half2float(sc_3), __half2float(sc_4), __half2float(sc_5), __half2float(sc_6), __half2float(sc_7));
-  //       *(reinterpret_cast<uint4*>(C + goffset_c * BYTES_PER_ELT)) = *(reinterpret_cast<uint4*>(smem_acc + ld_offset_c * BYTES_PER_ELT));
-  //   }
-  //   // t_a = t_b;
-    
-  //   // *(reinterpret_cast<uint4*>(c + offset)) = *(reinterpret_cast<uint4*>(smem_acc + row_acc * MMA_N * BYTES_PER_ELT + col_acc * ELTS_PER_THREADS * BYTES_PER_ELT));
-  // // }
+  uint4 t_a = *(reinterpret_cast<uint4*>(C + goffset_c));
+  if (thread_coordinate_c[0] < threads_layout_c[0]) {
+    #ifdef DEBUG_WARP_MATMUL_DEVICE
+          // printf("WarpMatmulForwardV8: thread_coordinate_c: [%d, %d], goffset_c size is: %d, ld_offset_c is %d\n", 
+          //         thread_coordinate_c[0], thread_coordinate_c[1], goffset_c, ld_offset_c);
+          // __half sc_0 = *reinterpret_cast<__half*>(smem_acc + 4 * groupID *4 + 4* threadID_in_group);
+          // __half sc_1 = *reinterpret_cast<__half*>(smem_acc + 4 * groupID *4 + 4* threadID_in_group + 2);
+          // __half sc_2 = *reinterpret_cast<__half*>(smem_acc + 32 * 4 + 4 * groupID *4 + 4* threadID_in_group);
+          // __half sc_3 = *reinterpret_cast<__half*>(smem_acc + 32 * 4 + 4 * groupID *4 + 4* threadID_in_group + 2);
+          // printf("Tile_m: %d, Tile_n %d, thread_coordinate_c: [%d, %d], goffset_c size is: %d, ld_offset_c is %d, sc_0 %f sc_1 %f sc_2 %f sc_3 %f\n",
+          //         Tile_m, Tile_n, thread_coordinate_c[0], thread_coordinate_c[1], goffset_c, ld_offset_c, __half2float(sc_0),  __half2float(sc_1), __half2float(sc_2), __half2float(sc_3));
+    #endif
+    *(reinterpret_cast<uint4*>(C + goffset_c)) = *(reinterpret_cast<uint4*>(smem_acc + ld_offset_c));
+  }
 }
-
 
 template <typename Warp_traits>
 void warp_matmul_v8_cuda(const torch::Tensor C, const torch::Tensor A, torch::Tensor B, const int M, const int N, const int K) {
@@ -264,7 +250,7 @@ void warp_matmul_v8_cuda(const torch::Tensor C, const torch::Tensor A, torch::Te
   std::cout << "sharedMemPerBlock: " << prop.sharedMemPerBlock 
             << " sharedMemPerMultiprocessor: " << prop.sharedMemPerMultiprocessor
             << " sharedMemPerBlockOptin: " << prop.sharedMemPerBlockOptin << std::endl;
-  int smem_size = 1024 * 48;
+  int smem_size = ((M * N + M * K + N * K) * sizeof(elem_type) + 1024 - 1) / 1024;
   cudaFuncSetAttribute(
                 WarpMatmulForwardV8<Warp_traits>, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
   printf("threads is (%d, %d, %d); blocks is (%d, %d, %d)\n", threads.x, threads.y, threads.z, blocks.x, blocks.y, blocks.z);
--- ld_mma/tests/warp_matmul_test.py
+++ ld_mma/tests/warp_matmul_test.py
@@ -3,17 +3,24 @@ import torch
 import ld_mma
 from ld_mma.ld_mma_interface import warp_matmul
 
-M = 16
-N = 8
-K = 16
+torch.set_printoptions (precision=6)
+torch.manual_seed(0)
+
+M = 16 * 1
+N = 8 * 1
+K = 16 * 1025
 
 device = 'cuda'
 dtype = torch.float16
 
-A = torch.randn((M, K), device='cuda', dtype=dtype, requires_grad=False)
-B = torch.randn((K, N), device='cuda', dtype=dtype, requires_grad=False)
+A = torch.ones((M, K), device='cuda', dtype=dtype, requires_grad=False)
+B = torch.ones((K, N), device='cuda', dtype=dtype, requires_grad=False)
 C = torch.empty((M, N), device='cuda', dtype=dtype, requires_grad=False)
 
 warp_matmul(C, A, B)
 
 print(f'C: {C}')
+
+D = torch.matmul(A, B)
+
+print(f'D: {D}')
